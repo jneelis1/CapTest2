70551251ebe4d1a4ff09d89735c237cec3124f37
mneel <neelismi@msu.edu>
2020-11-02 21:09:58 -0500

    17th commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var _path = _interopRequireDefault(require("path"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production' ) {
   require('dotenv').config();
 }
 
