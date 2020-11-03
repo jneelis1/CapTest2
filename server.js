780c1a5425a9e05474d65060c91596aba962985b
mneel <neelismi@msu.edu>
2020-11-02 20:44:09 -0500

    10th commit

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
-"use strict";
+import path from 'path';
 
-var _path = _interopRequireDefault(require("path"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+  require('dotenv').config()
 }
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+var mockScrapes = require('./mock_scrapes')
 
-var express = require('express');
+const app = express()
 
-var bodyParser = require('body-parser');
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-var cors = require('cors');
-
-var mockScrapes = require('./mock_scrapes');
-
-var app = express();
-app.use(express["static"](_path["default"].join(__dirname, 'client', 'build')));
-
-var db = require('./app/models'); // db.sequelize.sync({ force: true }).then(() => {
+const db = require('./app/models')
+// db.sequelize.sync({ force: true }).then(() => {
 //   return mockScrapes.forEach(mockScrape => {
 //     db.scrape
 //       .create(mockScrape)
@@ -28,12 +23,13 @@ var db = require('./app/models'); // db.sequelize.sync({ force: true }).then(() 
 //       )
 //   })
 // })
+
 // var corsOptions = {
 //   origin: 'localhost:3000'
 // }
 
-
-app.use(cors()); // app.use(function(req, res, next) {
+app.use(cors())
+// app.use(function(req, res, next) {
 //   res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
 //   res.header(
 //     'Access-Control-Allow-Headers',
@@ -41,24 +37,22 @@ app.use(cors()); // app.use(function(req, res, next) {
 //   )
 //   next()
 // })
+
 // parse requests of content-type - application/json
+app.use(bodyParser.json())
 
-app.use(bodyParser.json()); // parse requests of content-type - application/x-www-form-urlencoded
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(bodyParser.urlencoded({
-  extended: true
-})); // simple route
+// simple route
+app.get('/', (req, res) => {
+  res.json({ message: 'Web scraper API v1.1' })
+})
 
-app.get('/', function (req, res) {
-  res.json({
-    message: 'Web scraper API v1.1'
-  });
-});
+require('./app/routes/scrape.routes')(app)
 
-require('./app/routes/scrape.routes')(app); // set port, listen for requests
-
-
-var PORT = process.env.PORT || 8080;
-app.listen(PORT, function () {
-  console.log("Server is running on port ".concat(PORT, "."));
-});
+// set port, listen for requests
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
+})
